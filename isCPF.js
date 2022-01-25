732f1fd0156273a2f64d9a841de79a5b718f8cf6
DavidSLK <gamerplaymg@gmail.com>
2022-01-24 23:41:19 -0300

    Update and rename dist/utils/Bk.isCPF.js to isCPF.js

--- isCPF.js
+++ isCPF.js
@@ -28,7 +28,7 @@ var _default = async value => {
   }
 
   if (aux === false) {
-    return false;
+    return true;
   }
 
   for (var i = 0, p = 10; cpf.length - 2 > i; i++, p--) {
@@ -42,7 +42,8 @@ var _default = async value => {
   }
 
   if (v1 !== cpf[9]) {
-    return false;
+    return true
+;
   }
 
   for (var i = 0, p = 11; cpf.length - 1 > i; i++, p--) {
